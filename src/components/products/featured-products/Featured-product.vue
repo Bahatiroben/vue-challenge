16b315b82ec51ae70208098c1fce3b9c904daa7e
BAHATI Robben <bahatiroben@gmail.com>
2022-11-20 14:20:46 +0200

    upgrating the products hover effect

--- src/components/products/featured-products/Featured-product.vue
+++ src/components/products/featured-products/Featured-product.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
-    <img :src="imageUrl"/>
+    <img class="main-image" :src="imageUrl"/>
+    <div class="overlay-container">
+    <div class="overlay">
+      <h3>Lorem ipsum dolor sit</h3>
+      <img class="logo" src="../../../assets/logos/brignoli-grey.svg"/>
+      <div class="price">from $12/meter</div>
+      <div class="circles">
+        <div class="circle red"></div>
+        <div class="circle grey"></div>
+        <div class="circle green"></div>
+        <div class="circle black"></div>
+        <div>+20</div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -15,9 +29,87 @@ export default {
 <style scoped>
   .container {
     width: 100%;
+    display: block;
+    position: relative;
   }
 
-  img {
+  h3 {
+    font-weight: 700;
+    margin-top: 15px;
+  }
+
+  .main-image {
     width: 100%
+  }
+
+  .price {
+    margin-bottom: 15px;
+  }
+
+  .logo {
+    opacity: 1;
+    width: 70px;
+    padding: 10px;
+  }
+
+  .overlay-container {
+    position: absolute;
+    z-index: 1;
+    min-height: 100%;
+    width: 100%;
+    top: 0;
+    background-color: transparent;
+    opacity: 0;
+  }
+
+  .overlay-container:hover {
+    opacity: 1
+  }
+
+  .overlay {
+    background-color: white;
+    width: 100%;
+    font-size: 14px;
+    position: absolute;
+    top: 62%;
+    min-height: 34%;
+  }
+
+  .overlay:hover {
+    opacity: 1;
+  }
+
+  .overlay-hidden {
+
+  }
+
+  .circles {
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    margin: auto;
+    font-size: 14px;
+  }
+
+  .circle {
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+  }
+
+  .red {
+    background-color: maroon;
+  }
+
+  .grey {
+    background-color: grey;
+  }
+
+  .green {
+    background-color: rgb(13, 66, 13);
+  }
+
+  .black {
+    background-color: black;
   }
 </style>
